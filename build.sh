b06a449724e87937f365054b02df4cd28d2ce1b8
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2024-11-13 14:37:45 +0000

    Separate dist folders for firefox and chrome

--- build.sh
+++ build.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
-#
 
-DIST_DIR=./dist
+# Define directories
+FIREFOX_DIST_DIR=./dist/firefox
+CHROME_DIST_DIR=./dist/chrome
 
-if [[ ! -d $DIST_DIR ]]
-then
-  echo "$DIST_DIR doesn't exist"
-  exit
-fi
+# Create directories if they don't exist
+mkdir -p $FIREFOX_DIST_DIR
+mkdir -p $CHROME_DIST_DIR
 
+# Copy common files to both directories
 cp -v \
-  manifest.json \
   static/* \
-  $DIST_DIR/
+  $FIREFOX_DIST_DIR/
+cp -v \
+  static/* \
+  $CHROME_DIST_DIR/
+
+# Copy specific manifest files
+cp -v manifest-firefox.json $FIREFOX_DIST_DIR/manifest.json
+cp -v manifest-chrome.json $CHROME_DIST_DIR/manifest.json
+
+# Copy build output to both directories
+cp -v dist/sindicator.js $FIREFOX_DIST_DIR/
+mv -v dist/sindicator.js $CHROME_DIST_DIR/
+cp -v dist/options.js $FIREFOX_DIST_DIR/
+mv -v dist/options.js $CHROME_DIST_DIR/
+
+echo "Build files copied to $FIREFOX_DIST_DIR and $CHROME_DIST_DIR"
